020183fcab891eff2f80269a951603a6c6e0f246
Larissa Carolina <laholiveira001@gmail.com>
2025-09-18 19:25:42 -0300

    cruds

---
+++ projeto/app/Models/Areaspatio.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class areaspatio extends Model
+{
+    protected $table = "areaspatio";
+    protected $fillable = ["descricao"];
+}
--- projeto/routes/web.php
+++ projeto/routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FuncaoVisitanteController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('funcaovisitante', FuncaoVisitanteController::class);
+
+
+
+
+Route::resource('transportadora', TransportadoraController::class);
+
+
+Route::resource('motorista', MotoristaController::class);
+
+Route::resource('motorista', AreaspatioController::class);
